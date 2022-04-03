97f2e33d21278f18879283b9a769d040038cc5b8
Red S <redstreet@users.noreply.github.com>
2022-04-03 16:24:11 -0700

    use new querytable, and bump up corresponding fava version required

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'beancount>=2.3.1',
-        'fava>=1.13',
+        'fava>=1.19',
         'argh>=0.23.3',
         'argcomplete>=1.11.1'
     ],
